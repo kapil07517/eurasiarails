8693278ebc64d45849fac0b6048ab597ccc272c2
neeraji2it <neeraji2it@gmail.com>
2014-04-22 16:21:30 +0530

    added live facebook credentials

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,13 +1,5 @@
 class HomeController < ApplicationController
   
   def index
-    my_app = FbGraph::Application.new(FB_APP_ID);
-    acc_tok = my_app.get_access_token(FB_SEC_ID);
-    me = FbGraph::User.me(current_user.token)
-    me.fetch
-    me.accounts
-    account = me.accounts.select {|account| account if account.name == "kapil07517"}.first
-    page = FbGraph::Page.new(current_user.uid)
-    puts page.link
   end
 end
